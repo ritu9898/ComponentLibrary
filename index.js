fca5e9edf0ca855e2034d14c71bb5e157819b4a4
ritu <ritumkapadia@gmail.com>
2022-04-18 00:10:16 +0530

    Added rating component

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ $('.dismiss').on('click', function(){
 $('.alert .close').on('click', function(){
   $(this).parent()[0].style.display = 'none';
 });
+
+$(document).ready(function(){
+  $('.rate i').hover(function(){
+    let children = $(this).parent().children();
+    children.map((f) => {
+      if(parseInt(children[f].id) <= parseInt(this.id)) 
+      {
+        children[f].classList.add('gold');
+      } else {
+        children[f].classList.remove('gold');
+      }
+    });
+  });
+
+  $('.rate span').on('click', function(){
+    this.classList.add('green');  
+  });
+});
